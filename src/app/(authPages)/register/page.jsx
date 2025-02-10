117ae1a3140c7efe48d2aa839dac3642b1e618b2
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-10 22:32:48 +0600

    added the form

--- src/app/(authPages)/register/page.jsx
+++ src/app/(authPages)/register/page.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 export default function RegisterPage() {
   return (
-    <div>
-      <h3>this is register page</h3>
-    </div>
-  )
+    <section>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <Image
+              src={"../../../../public/assets/images/login/login.svg"}
+              width={100}
+              height={100}
+              alt="logo"
+            ></Image>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form className="card-body">
+              <h1 className="font-bold text-3xl text-center">Sign Up</h1>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                 
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary bg-red-600">Sign Up</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
