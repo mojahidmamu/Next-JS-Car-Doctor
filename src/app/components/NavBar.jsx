3ed1306f09692c659966dba4684e0a8089f48c3f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-10 12:02:33 +0600

    added logo icon

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 export default function NavBar() {
@@ -38,7 +39,9 @@ export default function NavBar() {
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">
+            <Image src={'/assets/logo.svg'} width={60} height={60} ></Image>
+          </a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
