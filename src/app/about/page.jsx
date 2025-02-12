7810011f952aa7a118afb4418d0f989dbcd523ff
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-12 07:52:25 +0600

    added two new file and folder

---
+++ src/app/about/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function aboutPage() {
+  return (
+    <div>
+      <h2>tihs is about page</h2>
+    </div>
+  )
+}
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ export default function NavBar() {
       <>
         <li> <Link href={'/'}> Home</Link> </li>
         <li> <Link href={'/about'}> About</Link> </li>
-        <li> <Link href={'/services'}> service</Link> </li>
+        <li> <Link href={'/service'}> service</Link> </li>
         <li> <Link href={'/blogs'}> Blogs</Link> </li>
         <li> <Link href={'/contacts'}> Contacts </Link> </li> 
       </>
