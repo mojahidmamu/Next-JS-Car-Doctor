43505550a07f9039aabafc0a5422a30b87ba2bf7
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-12 08:46:50 +0600

    added

--- src/app/contacts/page.jsx
+++ src/app/contacts/page.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
 
 export default function ContactsPage() {
   return (
     <div>
-      <h1>this is contacts page</h1>
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl  my-10">
+        <form className="card-body ">
+            <h1 className="text-3xl font-bold text-center">Contacts</h1>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Your Name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Address</span>
+            </label>
+            <input
+              type="text"
+              placeholder="address"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Phone Number</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Phone"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Call </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
